2332656e41cfc44103cd8db06fe7dcddb613e0b4
Clay Wood <clay.wood@gmail.com>
2017-01-21 19:20:22 +0000

    First commit

---
+++ js/wall.js
@@ -0,0 +1,13 @@
+var WALL_SIZE = EDGE_SIZE * 2;
+
+// Wall Colors.
+var NORMAL_WALL_COLOR = "#000000";
+var GLASS_WALL_COLOR = "#E59400";
+var DOOR_COLOR = "#000000";
+
+var WallType = {
+	NORMAL: "normal",
+	GLASS: "glass",
+	DOOR: "door"
+}
+
