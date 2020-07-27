3a09bbfe2374901bc46842fcfce003f23d9523f7
Bryan Dormaier <bryan.dormaier@gmail.com>
2020-07-26 23:28:12 -0700

    fix maplink hover

--- js/table_of_contents.js
+++ js/table_of_contents.js
@@ -61,7 +61,7 @@ TableOfContents.prototype.draw = function() {
             html += "<ul><li><h6>" + map.set + "</h6></li>";
             currentSet = map.set;
         }
-		html += "<li class\"mapLink\">"
+		html += "<li class=\"mapLink\">"
         html += "<a href='' id='toc" + i +
             "' onclick='mgr.setMap(" + i +
             "); return false;'>" +
